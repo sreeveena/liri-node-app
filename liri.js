21717759e5234f034c8a2c7e29ca0e5544d24d65
sreeveena <sreeveena21@gmail.com>
2019-11-26 10:57:15 -0800

    installed all the required dependencies.

--- liri.js
+++ liri.js
@@ -2,18 +2,25 @@ require("dotenv").config();
 var axios = require("axios");
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
+var argv = process.argv;
 
+for(var i = 2; i < argv.length; i++){
+    if(argv[i] == concert-this){
+
+    }else if(argv[i] == spotify-this-song){
+
+    }else if(argv[i] == movie-this){
+
+    }else if(argv[i] == do-what-it-says){
+
+    }else{
+        console.log("Sorry this request cannot be made.");
+    }
+}
 
 axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
   function(response) {
     console.log("The movie's rating is: " + response.data.imdbRating);
   })
-var spotify = {
-    provider: "spotify",
-    apiKey: "ecab0bec1b9142ac910f8859ef8b6f58"
-  };
-  var bandsintown = {
-    provider: "bandsintown",
-    apiKey: "4abed1e6cf7aa9ccfe2a41418ef40917."
-  };
+
 
