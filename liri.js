11d6cd0ef8757cacbabb4967eb4574778251e4c3
sreeveena <sreeveena21@gmail.com>
2019-11-26 11:39:21 -0800

    worked on movie-this.

--- liri.js
+++ liri.js
@@ -1,26 +1,56 @@
 require("dotenv").config();
 var axios = require("axios");
 var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
-var argv = process.argv;
+// var spotify = new Spotify(keys.spotify);
+var nodeA = process.argv;
+var name = "";
+var request = nodeA[2].toLowerCase();
 
-for(var i = 2; i < argv.length; i++){
-    if(argv[i] == concert-this){
+function getName(){
+    for( var i=3; i<nodeA.length; i++){
+        if(i>3 && i< nodeA.length){
+            name = name + " " + nodeA[i];
+        }else{
+            name += nodeA[i];
+        }
+    
+    }
+}
 
-    }else if(argv[i] == spotify-this-song){
+function getRottenTomatoesRating(rating){
+    for(var i=0; i<rating.length; i++){
+        if(rating[i].Source == "Rotten Tomatoes"){
+            return rating[i].Value;
+        }
+    }
+return "0%";
+}
+// for(var i = 2; i < argv.length; i++){
+    if(request == "concert-this"){
 
-    }else if(argv[i] == movie-this){
+    }else if(request == "spotify-this-song"){
 
-    }else if(argv[i] == do-what-it-says){
+    }else if(request == "movie-this"){
+        getName();
+        var queryUrl = "http://www.omdbapi.com/?t=" + name + "&y=&plot=short&apikey=trilogy";
+        axios.get(queryUrl).then(
+            function(response) {
+              console.log("Title of the movie is: " + response.data.Title);
+              console.log("Year the movie came out: " + response.data.Year);
+              console.log("IMDB Rating of the movie: " + response.data.imdbRating);
+              console.log("Rotten Tomatoes Rating of the movie: " + getRottenTomatoesRating(response.data.Ratings));
+              console.log("Country where the movie was produced: " + response.data.Country);
+              console.log("Language of the movie: " + response.data.Language);
+              console.log("Plot of the movie: " + response.data.Plot);
+              console.log("Actors in the movie: " + response.data.Actors);
+            });
+    }else if(request == "do-what-it-says"){
 
     }else{
         console.log("Sorry this request cannot be made.");
     }
-}
+// }
 
-axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
-  function(response) {
-    console.log("The movie's rating is: " + response.data.imdbRating);
-  })
+
 
 
