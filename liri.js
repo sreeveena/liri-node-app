40bb6a8b51c0db0fd589f06a591c465890f32381
sreeveena <sreeveena21@gmail.com>
2019-11-26 22:44:08 -0800

    worked on spotify.

--- liri.js
+++ liri.js
@@ -9,7 +9,14 @@ var nodeA = process.argv;
 var name = "";
 //this variable will have user request 
 var request = nodeA[2].toLowerCase();
-
+/*
+var SpotifyWebApi = require('spotify-web-api-node');
+var spotifyApi = new SpotifyWebApi({
+    clientId: 'ecab0bec1b9142ac910f8859ef8b6f58',
+    clientSecret: '69359b9034d84ac89ca5beb3afb06cf8',
+    redirectUri: 'http://www.example.com/callback'
+  });
+*/
 //this function will get the user input value which represents the movie name
 //or band name 
 function getName(){
@@ -47,52 +54,44 @@ return "0%";
               
             });
     }else if(request == "spotify-this-song"){
+        // JSON data format from Spotify website:
+        // { 
+        //    album: { name: 'album-name', ...},
+        //    artists: [ { name: 'name', ...}, 
+        //               { name: 'name', ...}],
+        //    preview_url: 'https://...',
+        //    name: 'song-name',
+        //    ...
+        // //}
         getName();
         if(name == ""){
-            spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-                if (err) {
-                  return console.log('Error occurred: ' + err);
-                }
-                for( var i =0; i<response.data.length; i++){
-                    console.log("Artist(s): " +data.tracks.items[i].album.artists[i]);
-                    console.log("The song's name: " +data.tracks.items[i].album.artists[i]);
-                    console.log("A preview link of the song from Spotify: " +data.tracks.items[i].album.external_urls.spotif);
-                    console.log("The album that the song is from: " +data.tracks.items[i].album.artists[i]);    
-                } 
-
-            });
-       
-        }else{
-            spotify.search({ type: 'track', query: name }, function(err, data) {
-                if (err) {
-                  return console.log('Error occurred: ' + err);
-                }
-                for( var i =0; i<response.data.length; i++){
-                    console.log("Artist(s): " +data.tracks.items[i].album.artists[i]);
-                    console.log("The song's name: " +data.tracks.items[i].album.artists[i]);
-                    console.log("A preview link of the song from Spotify: " +data.tracks.items[i].album.external_urls.spotif);
-                    console.log("The album that the song is from: " +data.tracks.items[i].album.artists[i]);    
-                }  
-            });   
+            name = "The Sign";
         }
+        spotify.search({ type: 'track', query: "track:" + name, limit: 20}, function(err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        for( var i = 0; i < data.tracks.items.length; i++){
+            if(data.tracks.items[i].name == "The Sign"){
+                console.log("Artist(s):");
+                console.log(data.tracks.items[i].artists[0].name);
+                console.log("The song's name:");
+                console.log(data.tracks.items[i].name);
+                console.log("A preview link of the song from Spotify:");
+                console.log(data.tracks.items[i].preview_url);
+                console.log("The album that the song is from:");
+                console.log(data.tracks.items[i].album.name);
+                break;
+            }
+        }
+                        
+       });  
+        
+        
     }else if(request == "movie-this"){
         if(name == ""){
-            var queryUrl = "http://www.omdbapi.com/?t=Mr.+Nobody&y=&plot=short&apikey=trilogy";
-            axios.get(queryUrl).then(
-            function(response) {
-              console.log("Title of the movie is: " + response.data.Title);
-              console.log("Year the movie came out: " + response.data.Year);
-              console.log("IMDB Rating of the movie: " + response.data.imdbRating);
-              console.log("Rotten Tomatoes Rating of the movie: " + getRottenTomatoesRating(response.data.Ratings));
-              console.log("Country where the movie was produced: " + response.data.Country);
-              console.log("Language of the movie: " + response.data.Language);
-              console.log("Plot of the movie: " + response.data.Plot);
-              console.log("Actors in the movie: " + response.data.Actors);
-              console.log("If you haven't watched Mr. Nobody then you should: http://www.imdb.com/title/tt0485947/ ");
-              console.log("It's on Netflix! ");
-            });
-        }else{
-            
+            name = "Mr. Nobody";
+        }
             getName();
             var queryUrl = "http://www.omdbapi.com/?t=" + name + "&y=&plot=short&apikey=trilogy";
             axios.get(queryUrl).then(
@@ -106,7 +105,6 @@ return "0%";
               console.log("Plot of the movie: " + response.data.Plot);
               console.log("Actors in the movie: " + response.data.Actors);
             });
-        }
         
     }else if(request == "do-what-it-says"){
 
