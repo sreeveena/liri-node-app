6b1a120d6038d546644bcd19fc0af8e1f808d7f0
sreeveena <sreeveena21@gmail.com>
2019-11-27 12:52:23 -0800

    output the data to a log.txt file.

--- liri.js
+++ liri.js
@@ -11,9 +11,19 @@ var name = "";
 //this variable will have user request 
 var request;
 
+//generic log function that is used to write to console and a log file.
+function log(message){
+    console.log(message);
+    fs.appendFile("log.txt", message + '\n', function(err){
+        if(err){
+            console.log(err);
+        }
+    });
+}
 //this function will get the user input value which represents the movie name
 //or band name 
 function getName(){
+    name = "";
     for( var i=3; i<nodeA.length; i++){
         if(i>3 && i< nodeA.length){
             name = name + " " + nodeA[i];
@@ -41,10 +51,10 @@ function getConcertInfo(name){
     axios.get(queryUrl).then(
     function(response) {
         for( var i =0; i<response.data.length; i++){
-            console.log("Name of the venue: " + response.data[i].venue.name);
-            console.log("Venue location: " + response.data[i].venue.city + " "
+            log("Name of the venue: " + response.data[i].venue.name);
+            log("Venue location: " + response.data[i].venue.city + " "
                 + response.data[i].venue.region + " " + response.data[i].venue.country);
-            console.log("Date of the Event: " +  moment(response.data[i].datetime).format('MM/DD/YYYY')); 
+            log("Date of the Event: " +  moment(response.data[i].datetime).format('MM/DD/YYYY')); 
         }
         
     });
@@ -56,19 +66,19 @@ function getSongInfo(name){
     }
     spotify.search({ type: 'track', query: "track:" + name, limit: 20}, function(err, data) {
     if (err) {
-        return console.log('Error occurred: ' + err);
+        return log('Error occurred: ' + err);
     }
     for( var i = 0; i < data.tracks.items.length; i++){
-        // console.log(data.tracks.items[i].name);
+        // log(data.tracks.items[i].name);
         if(data.tracks.items[i].name == name){
-            console.log("Artist(s):");
-            console.log(data.tracks.items[i].artists[0].name);
-            console.log("The song's name:");
-            console.log(data.tracks.items[i].name);
-            console.log("A preview link of the song from Spotify:");
-            console.log(data.tracks.items[i].preview_url);
-            console.log("The album that the song is from:");
-            console.log(data.tracks.items[i].album.name);
+            log("Artist(s):");
+            log(data.tracks.items[i].artists[0].name);
+            log("The song's name:");
+            log(data.tracks.items[i].name);
+            log("A preview link of the song from Spotify:");
+            log(data.tracks.items[i].preview_url);
+            log("The album that the song is from:");
+            log(data.tracks.items[i].album.name);
             break;
         }
     }
@@ -80,30 +90,45 @@ function getSongInfo(name){
 function getMovieInfo(name){
     if(name == ""){
         name = "Mr. Nobody";
-        console.log("If you haven't watched Mr. Nobody, then you should: http://www.imdb.com/title/tt0485947/");
-        console.log("It's on Netflix!");
+        log("If you haven't watched Mr. Nobody, then you should: http://www.imdb.com/title/tt0485947/");
+        log("It's on Netflix!");
     }
     var queryUrl = "http://www.omdbapi.com/?t=" + name + "&y=&plot=short&apikey=trilogy";
     axios.get(queryUrl).then(
     function(response) {
-        console.log("Title of the movie is: " + response.data.Title);
-        console.log("Year the movie came out: " + response.data.Year);
-        console.log("IMDB Rating of the movie: " + response.data.imdbRating);
-        console.log("Rotten Tomatoes Rating of the movie: " + getRottenTomatoesRating(response.data.Ratings));
-        console.log("Country where the movie was produced: " + response.data.Country);
-        console.log("Language of the movie: " + response.data.Language);
-        console.log("Plot of the movie: " + response.data.Plot);
-        console.log("Actors in the movie: " + response.data.Actors);
+        log("Title of the movie is: " + response.data.Title);
+        log("Year the movie came out: " + response.data.Year);
+        log("IMDB Rating of the movie: " + response.data.imdbRating);
+        log("Rotten Tomatoes Rating of the movie: " + getRottenTomatoesRating(response.data.Ratings));
+        log("Country where the movie was produced: " + response.data.Country);
+        log("Language of the movie: " + response.data.Language);
+        log("Plot of the movie: " + response.data.Plot);
+        log("Actors in the movie: " + response.data.Actors);
     });
     
 }
-// console.log(nodeA[3]);
+// log(nodeA[3]);
 if(typeof nodeA[2] != 'undefined'){
     request = nodeA[2].toLowerCase();
 }
+
+//adding the user request in a log.txt file
+getName();
+var userRequest = (request + " "+ name + '\n');
+// log(userRequest);
+fs.appendFile("log.txt", userRequest, function(err) {
+
+    // If an error was experienced we will log it.
+    if (err) {
+      log(err);
+    }
+});
+  
+   
  // this if else conditions are to check what the user request is and give the appropriate result
     if(request == "concert-this"){
         getName();
+        log(name);
         getConcertInfo(name);
         
     }else if(request == "spotify-this-song"){
@@ -117,13 +142,13 @@ if(typeof nodeA[2] != 'undefined'){
         var randomN = Math.floor(Math.random()*19)+1;
         fs.readFile("random.txt", "utf8", function(err,data){
             if(err){
-                return console.log(err); 
+                return log(err); 
             }
             var dataArr = data.split("\n");
             var line = dataArr[randomN];
             var lineArr = line.split(",");
-            console.log (lineArr[0]);
-            console.log(lineArr[1]);
+            log (lineArr[0]);
+            log(lineArr[1]);
             request = lineArr[0];
             name = lineArr[1];
             if(request == "concert-this"){
@@ -138,9 +163,8 @@ if(typeof nodeA[2] != 'undefined'){
         });
 
     }else{
-        console.log("Sorry this request cannot be made.");
+        log("Sorry this request cannot be made.");
     }
-
 
 
 
